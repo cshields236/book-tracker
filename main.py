19653c72e5e5d5c3d3678ad86f95134ec62d6bb8
Conor Shields <conor.shields1@mydit.ie>
2020-11-18 15:24:28 +0000

    adding and showing book

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from firebase import  firebase
+from firebase import firebase
+import datetime
 
-def connect():
-    firebase = firebase.FirebaseApplication('https://book-tracker-696b0.firebaseio.com', None)
+firebase = firebase.FirebaseApplication(
+    'https://book-tracker-696b0.firebaseio.com', None)
 
-# result = firebase.get('/Test', None)
-# print(result)
+
+def show_books():
+    result = firebase.get('/books', None)
+    print(result)
+
 
 def add_book(Title, Author, Genre, StartDate, EndDate, Pages, Progress, Finished):
-    new_book = [Title, Author, Genre, StartDate, EndDate, Pages, Progress, Finished]
+    new_book = {
+        'Title': Title, 'Author': Author, 'Genre': Genre, 'Start Date': StartDate, 'End Date': EndDate, 'Pages': Pages, 'Progress': Progress, 'Finished': Finished
+    }
     result = firebase.post('/books', new_book)
-    print(result)
+    print(result == None)
+
+
+# add_book('Champagne Football', 'Mark Tighe', 'Sport', datetime.datetime(
+#     2020, 5, 17), datetime.datetime(2020, 5, 19), 350, 350, True)
+
+show_books()

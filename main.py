449dbf3a2b8de1432f8afffd77ce3131d051efa9
Conor Shields <conor.shields1@mydit.ie>
2020-11-18 16:40:20 +0000

    changed to mysql

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-from firebase import firebase
+import mysql.connector
 import datetime
 
-firebase = firebase.FirebaseApplication(
-    'https://book-tracker-696b0.firebaseio.com', None)
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='root',
+    database='booktracker'
+)
 
-
-def show_books():
-    result = firebase.get('/books', None)
-    print(result)
-
+mycursor = mydb.cursor()
 
 def add_book(Title, Author, Genre, StartDate, EndDate, Pages, Progress, Finished):
-    new_book = {
-        'Title': Title, 'Author': Author, 'Genre': Genre, 'Start Date': StartDate, 'End Date': EndDate, 'Pages': Pages, 'Progress': Progress, 'Finished': Finished
-    }
-    result = firebase.post('/books', new_book)
-    print(result == None)
+    formatted_date = StartDate.strftime('%Y-%m-%d')
+    formatted_date = EndDate.strftime('%Y-%m-%d')
+    sql = 'INSERT INTO books (title, author, genre, startdate, enddate, pages, progress, finished) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) '
+    new_book = (Title,  Author,  Genre,  StartDate,
+                EndDate, Pages,  Progress,  Finished)
+
+    mycursor.execute(sql, new_book)
+    mydb.commit()
 
 
-# add_book('Champagne Football', 'Mark Tighe', 'Sport', datetime.datetime(
-#     2020, 5, 17), datetime.datetime(2020, 5, 19), 350, 350, True)
-
-show_books()
+add_book('Champagne Football', 'Mark Tighe', 'Sport', datetime.datetime(
+    2020, 5, 17), datetime.datetime(2020, 5, 19), 350, 350, True)

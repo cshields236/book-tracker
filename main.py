6ae0d4e65faca7371b74bc171fd77f08491f31b3
Conor Shields <conor.shields1@mydit.ie>
2020-11-20 14:25:21 +0000

    adding show all books and show by filter

--- main.py
+++ main.py
@@ -20,6 +20,33 @@ def add_book(Title, Author, Genre, StartDate, EndDate, Pages, Progress, Finished
     mycursor.execute(sql, new_book)
     mydb.commit()
 
+def show_books():
+    mycursor.execute('SELECT * FROM books WHERE Genre="Crime"')
+    myresult = mycursor.fetchall()
 
-add_book('Champagne Football', 'Mark Tighe', 'Sport', datetime.datetime(
-    2020, 5, 17), datetime.datetime(2020, 5, 19), 350, 350, True)
+    for book in myresult: 
+        print(book)
+
+def show_finished(): 
+    mycursor.execute('SELECT * FROM books WHERE finished=1')
+    myresult = mycursor.fetchall()
+
+    for book in myresult: 
+        print(book)
+
+
+def show_titles(): 
+    mycursor.execute('SELECT title FROM books')
+    myresult = mycursor.fetchall()
+
+    for book in myresult: 
+        print(book)
+
+def show_genres(): 
+    mycursor.execute('SELECT genre FROM books')
+    myresult = mycursor.fetchall()
+
+    for book in myresult: 
+        print(book)
+
+show_titles()
